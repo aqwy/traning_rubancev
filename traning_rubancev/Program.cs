28502b62432579c577fff1707be50458f0713f58
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-14 15:22:43 +0300

    Добавьте файлы проекта.

---
+++ traning_rubancev/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traning_rubancev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
